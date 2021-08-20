7f0f4fc14919c3c4bbaf5677faa2e087f0969e3e
marco.figueiredo@alfasoft.pt <marco.figueiredo@alfasoft.pt>
2021-08-20 00:25:48 -0300

    Adicionando Models

---
+++ Fullbar/Fullbar.Entities/Models/Enrollments/Enrollment.cs
@@ -0,0 +1,35 @@
+﻿using Fullbar.Entities.Models.Courses;
+using Fullbar.Entities.Models.Disciplines;
+using Fullbar.Entities.Models.Students;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fullbar.Entities.Models.Enrollments
+{
+	static class StudentStatus
+	{
+		public const string Approved = "Approved";
+		public const string Reproved = "Reproved";
+	}
+
+	public class Enrollment
+	{
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public int DisciplineID { get; set; }
+
+        public string Status
+        {
+            get
+            {
+                return StudentStatus.Approved;
+            }
+        }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+        public Discipline Discipline { get; set; }
+    }
+}
