f00689473e197f92eacb07ae1a6b0c9c59383aba
Jhonatas Lima <jhonatas.hl@defensoria.to.def.br>
2021-12-23 22:09:16 -0300

    Create Domain project

---
+++ Domain/Entities/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public class Course : EntityBase
+    {
+        public Course()
+        {
+            Subjects = new List<Subject>();
+        }
+        public string Name { get; set; }
+
+        public List<Subject> Subjects { get; set; }
+    }
+}
---
+++ Domain/Entities/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Subject : EntityBase
+    {
+        public string Name { get; set; }
+        public Guid CourseId { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal MinimumApprovalGrade { get; set; }
+
+        public Course Course { get; set; }
+
+        public List<SchoolReport> SchoolReports { get; set; }
+
+    }
+}
