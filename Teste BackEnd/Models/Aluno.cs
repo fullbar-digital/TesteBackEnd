2ca6988c3a97ace4722d0eb668f019d793f87a44
giovanimirandola <giovanimirandola2@gmail.com>
2022-08-07 20:06:34 -0300

    Correções Finais

--- Teste BackEnd/Models/Aluno.cs
+++ Teste BackEnd/Models/Aluno.cs
@@ -9,7 +9,6 @@ namespace Teste_BackEnd.Models
         [Required]
         public string Periodo { get; set; }
         public int CursoId { get; set; }
-        [Required]
         public Curso Curso { get; set; }
         public string Status { get; set; }
         public string Foto { get; set; }
